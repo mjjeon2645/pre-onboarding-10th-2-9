e9f11f3f434470b012e166e4b337e325eadb8cb6
Jeon Minji <mjjeon2645@gmail.com>
2023-05-03 23:26:37 +0900

    feat: 로컬 캐싱 구현을 위해 CacheStorage 활용

--- src/api/relatedKeywords.ts
+++ src/api/relatedKeywords.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 import ApiUrl from './ApiUrl';
 
 // TODO: CORS 에러 해결을 위해 instance 사용을 중단함. 추후 결정하기
@@ -11,10 +9,44 @@ import ApiUrl from './ApiUrl';
 //   return data;
 // };
 
-const fetchRelatedKeywords = async (keyword: string) => {
-  const { data } = await axios.get(`${ApiUrl.relatedKeywords}${keyword}`);
+const getCachedData = async (cacheName: string, url: string) => {
+  const cacheStorage = await caches.open(cacheName);
+  const cachedResponse = await cacheStorage.match(url);
 
-  return data;
+  if (!cachedResponse || !cachedResponse.ok) {
+    return false;
+  }
+
+  const result = await cachedResponse.json();
+
+  return result;
+};
+
+const getData = async (keyword: string) => {
+  const cacheName = 'relatedKeywords';
+  const URL = `${ApiUrl.relatedKeywords}${keyword}`;
+
+  const cachedData = await getCachedData(cacheName, URL);
+
+  if (cachedData) {
+    return cachedData;
+  }
+
+  const cacheStorage = await caches.open(cacheName);
+  await cacheStorage.add(URL);
+
+  const result = await getCachedData(cacheName, URL);
+
+  return result;
+};
+
+const fetchRelatedKeywords = async (keyword: string) => {
+  try {
+    const data = await getData(keyword);
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 export default fetchRelatedKeywords;
