22d9d42b8a61b772762771e3e5855835010cd60f
Jeon Minji <mjjeon2645@gmail.com>
2023-05-03 16:14:16 +0900

    feat: CORS 에러 해결을 위해 http-proxy-middleware 설치 후 setupProxy 설정

--- src/api/relatedKeywords.ts
+++ src/api/relatedKeywords.ts
@@ -1,10 +1,19 @@
-import instance from '.';
+import axios from 'axios';
 
 import ApiUrl from './ApiUrl';
 
+// TODO: CORS 에러 해결을 위해 instance 사용을 중단함. 추후 결정하기
+// const fetchRelatedKeywords = async (keyword: string) => {
+//   const { data } = await instance.get(`${ApiUrl.relatedKeywords}${keyword}`, {
+//     headers: { 'Cache-Control': 'no-cache' },
+//   });
+//   console.info('calling api');
+//   return data;
+// };
+
 const fetchRelatedKeywords = async (keyword: string) => {
-  const { data } = await instance.get(`${ApiUrl.relatedKeywords}${keyword}`);
-  console.info('calling api');
+  const { data } = await axios.get(`${ApiUrl.relatedKeywords}${keyword}`);
+
   return data;
 };
 
